9c8045fdcc7dd18893c86f9e044402ed9302ee16
Nouveau ici <marco.gagnon@gmail.com>
2017-05-18 17:11:20 -0400

    Transforme le script en fonction qui sera appelée.

--- scripts/moodle.js
+++ scripts/moodle.js
@@ -1,7 +1,10 @@
 // Test de script distant pour Moodle
 
+function test(){
+alert('Cette alerte vient de GitHub!');
 var titre = document.querySelector("h2");
 var nom = prompt("Quel est votre nom ?");
 var newH2 = "Bonjour, " + nom + ". Heureux de vous rencontrer!";
 titre.innerHTML = newH2;
 titre.style.color = "red";
+}
